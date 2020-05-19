27f66b0ef4c98e1b8438351a2c42c09d75250d7f
Yuchen <happyzychjl@gmail.com>
2020-05-18 23:09:25 -0400

    MASTER: Added particle background for the portfolio page

--- frontend/pages/portfolio/index.tsx
+++ frontend/pages/portfolio/index.tsx
@@ -1,8 +1,26 @@
+import Head from "next/head";
+import { ReactElement } from "react";
+import Particles, { IParticlesParams } from "react-particles-js";
 import PageWrapper from "components/PageWrapper";
+import particles from "./particles.json";
+import {
+  ParticleBackground
+} from "./styles";
 
-function Portfolio() {
+function Portfolio(): ReactElement {
   return (
-    <div> My portfolio Page</div>
+    <>
+      <Head>
+        <title>Portfolio</title>
+        <script src="https://cdn.rawgit.com/progers/pathseg/master/pathseg.js" />
+      </Head>
+      <div>
+        <ParticleBackground>
+          <Particles params={particles as IParticlesParams} />
+        </ParticleBackground>
+        <div>My portfolio Page</div>
+      </div>
+    </>
   );
 }
 
---
+++ frontend/pages/portfolio/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ParticleBackground = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  z-index: -1;
+  
+  & div {
+    height: 100%;
+  }
+`;
